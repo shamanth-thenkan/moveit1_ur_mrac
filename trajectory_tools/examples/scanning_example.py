5aee76c62ae7193b639c164ee1bb5cf6e2317c2d
shamanth-thenkan <shamanth-thenkan@github.com>
2022-12-03 14:50:29 +0000

    updates

--- trajectory_tools/examples/scanning_example.py
+++ trajectory_tools/examples/scanning_example.py
@@ -24,19 +24,19 @@ start_srv_req.tsdf_params.voxel_length = 0.2
 start_srv_req.tsdf_params.sdf_trunc = 0.4
 start_srv_req.tsdf_params.min_box_values = Vector3(x=0.0, y=0.0, z=0.0)
 start_srv_req.tsdf_params.max_box_values = Vector3(x=0.0, y=0.0, z=0.0)
-start_srv_req.rgbd_params.depth_scale = 500
-start_srv_req.rgbd_params.depth_trunc = 0.25
+start_srv_req.rgbd_params.depth_scale =1000
+start_srv_req.rgbd_params.depth_trunc = 0.5
 start_srv_req.rgbd_params.convert_rgb_to_intensity = False
 
 stop_srv_req = StopReconstructionRequest()
-# stop_srv_req.archive_directory = '/dev_ws/src.reconstruction/'
+stop_srv_req.archive_directory = '/dev_ws/src.reconstruction/'
 global height
 height = 0.15
 name = str(height)
 stop_srv_req.mesh_filepath = f"/home/usuario/{name}.ply"
-# stop_srv_req.normal_filters = [NormalFilterParams(
-#                     normal_direction=Vector3(x=0.0, y=0.0, z=1.0), angle=90)]
-# stop_srv_req.min_num_faces = 1000
+stop_srv_req.normal_filters = [NormalFilterParams(
+                    normal_direction=Vector3(x=0.0, y=0.0, z=1.0), angle=90)]
+stop_srv_req.min_num_faces = 500
 
 
 
